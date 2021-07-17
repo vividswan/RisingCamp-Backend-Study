f106e735d145bbd5a34753b86816d090fc9dc9ba
vividswan <vividswan@naver.com>
2021-07-17 13:44:41 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/utils/ValidationRegex.java
@@ -0,0 +1,30 @@
+package com.example.demo.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidationRegex {
+    public static boolean isRegexEmail(String target) {
+        String regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+    public static boolean isRegexPassword(String target){
+        String regex = "^[A-Za-z0-9!@#$%]{8,16}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+    public static boolean isRegexPhoneNumber(String target){
+        String regex = "^(010|011)[\\s]?\\d{3,4}[\\s]?\\d{4}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+
+}
+
