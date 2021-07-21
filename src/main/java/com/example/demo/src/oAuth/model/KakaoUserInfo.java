55676f896de4f0793dc97316ae73e0c349b814eb
vividswan <vividswan@naver.com>
2021-07-22 00:35:19 +0900

    feat: Kakao OAuth 로그인 관련 Dto 생성

---
+++ src/main/java/com/example/demo/src/oAuth/model/KakaoUserInfo.java
@@ -0,0 +1,14 @@
+package com.example.demo.src.oAuth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class KakaoUserInfo {
+
+    Long id;
+    String email;
+    String nickname;
+
+}
